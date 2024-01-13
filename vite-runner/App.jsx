8511408cc30b4ed3e1b60b6ba0611d811ada4f20
sleepyheadp <17863965581@163.com>
2024-01-13 14:35:20 +0800

    feat: 02 rename react

--- vite-runner/App.jsx
+++ vite-runner/App.jsx
@@ -1,10 +1,13 @@
+/**@jsx YReact.createElement */
 // 通过函数组件的形式进行创建
-import React from "./core/react.js";
+import YReact from "./core/react.js";
 // const App = React.createElement("div", { id: "app" }, "hello ", "mini-react");
+const App = <div id="app">Hello mini-react jsx</div>;
+
 // 函数式组件-jsx写法
-// function App() {
+// function AppOne() {
 // 	return <div id="app">Hello mini-react jsx</div>;
 // }
-const App = <div id="app">Hello mini-react jsx</div>;
-console.log(App); // () => {return /* @__PURE__ */ React.createElement("div", { id: "app" }, "Hello mini-react jsx");}
+// console.log(AppOne); // () => {return /* @__PURE__ */ React.createElement("div", { id: "app" }, "Hello mini-react jsx");}
+
 export default App;
