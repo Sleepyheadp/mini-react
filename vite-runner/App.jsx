a15ebdffcd924a0b1f892aeb2e53467af918b7a8
sleepyheadp <17863965581@163.com>
2024-01-14 22:28:57 +0800

    feat: 02 use fiber

--- vite-runner/App.jsx
+++ vite-runner/App.jsx
@@ -2,7 +2,7 @@
 // 通过函数组件的形式进行创建
 import YReact from "./core/react.js";
 // const App = React.createElement("div", { id: "app" }, "hello ", "mini-react");
-const App = <div id="app">Hello mini-react jsx</div>;
+const App = <div id="app">Hello mini-react fiber</div>;
 
 // 函数式组件-jsx写法
 // function AppOne() {
--- vite-runner/core/react.js
+++ vite-runner/core/react.js
@@ -1,28 +1,3 @@
-// v1:新建一个dom节点(#app)插入到body(#root)中,并且将其内容设置为hello mini-react
-// const dom = document.createElement('div')
-// dom.id = 'app'
-// document.querySelector('#root').append(dom)
-// // 创建一个文本节点赋值并插入
-// const textNode = document.createTextNode('')
-// textNode.nodeValue = 'hello mini-react v1'
-// dom.append(textNode)
-
-// v2: 动态创建vdom
-// const textDom = {
-// 	type: "text",
-// 	props: {
-// 		nodeValue: "hello mini-react v2",
-// 		children: [],
-// 	},
-// };
-// const vDom = {
-// 	type: "div",
-// 	props: {
-// 		id: "app",
-// 		children: [textDom],
-// 	},
-// };
-// 通过函数组件的形式进行创建
 function createTextNode(text) {
 	return {
 		type: "TEXT_ELEMENT",
@@ -45,36 +20,91 @@ function createElement(type, props, ...children) {
 	};
 }
 
-// const dom = document.createElement(App.type);
-// dom.id = App.props.id;
-// document.querySelector("#root").append(dom);
-// // 创建一个文本节点赋值并插入
-// const textNode = document.createTextNode("");
-// textNode.nodeValue = textDom.props.nodeValue;
-// dom.append(textNode);
-
 // v3:动态创建dom节点
 function render(el, container) {
-	// 第一步:创建dom节点
-	const dom =
-		el.type === "TEXT_ELEMENT"
-			? document.createTextNode("")
-			: document.createElement(el.type);
-	// 第二步:设置dom节点的属性值
-	Object.keys(el.props).forEach((key) => {
+	// 渲染的时候我们需要将vdom转换为dom节点
+	nextWorkOfUnit = {
+		dom: container,
+		props: {
+			children: [el],
+		},
+	};
+}
+
+// 首先我们定义一个当前要执行的任务
+let nextWorkOfUnit = null;
+const workLoop = (idleDeadline) => {
+	let hasTimeEmpty = false; // 默认没有空闲时间
+	while (!hasTimeEmpty && nextWorkOfUnit) {
+		// 当有空闲时间时执行后续操作
+		// 当前的任务指向下一个任务
+		nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit);
+		hasTimeEmpty = idleDeadline.timeRemaining() < 1;
+	}
+	requestIdleCallback(workLoop);
+};
+
+// 抽离createDom
+function createDom(type) {
+	return type === "TEXT_ELEMENT"
+		? document.createTextNode("")
+		: document.createElement(type);
+}
+// 抽离处理props函数
+function updateProps(dom, props) {
+	Object.keys(props).forEach((key) => {
 		if (key !== "children") {
-			dom[key] = el.props[key];
+			dom[key] = props[key];
 		}
 	});
-	// 第三步:将dom节点插入到container父节点中
-	const children = el.props.children;
-	// textEL append to divEl
-	children.forEach((child) => {
-		render(child, dom);
-	});
-	// #app El apped to #root El
-	container.append(dom);
 }
+// 转换链表
+function initChildren(fiber) {
+	const children = fiber.props.children;
+	let prevChild = null;
+	children.forEach((child, index) => {
+		// 创建一个新的fiber节点,给其设置相关的属性
+		const newFiber = {
+			type: child.type,
+			props: child.props,
+			child: null,
+			parent: fiber,
+			sibling: null,
+			dom: null,
+		};
+		if (index === 0) {
+			fiber.child = newFiber;
+		} else {
+			prevChild.sibling = newFiber;
+		}
+		prevChild = newFiber;
+	});
+}
+const performWorkOfUnit = (fiber) => {
+	// 在这个函数中我们要做的事情就是执行当前任务,也就是render函数所做的事情
+	// 1. 创建dom节点
+	if (!fiber.dom) {
+		const dom = (fiber.dom = createDom(fiber.type));
+		fiber.parent.dom.append(dom);
+		// 2. 处理props
+		updateProps(dom, fiber.props);
+	}
+	// 3. 转换链表 设置引用/指针(指向下一个任务)
+	initChildren(fiber);
+	// 4. 返回下一个要执行的任务(指针指向的下一个任务)
+	// 4.1 如果当前任务有子节点,则返回子节点
+	if (fiber.child) {
+		return fiber.child;
+	}
+	// 4.2 如果当前任务没有子节点,则返回兄弟节点
+	if (fiber.sibling) {
+		return fiber.sibling;
+	}
+	// 4.3 如果当前任务没有兄弟节点,则返回父节点的兄弟节点
+	return fiber.parent?.sibling;
+};
+requestIdleCallback(workLoop);
+
 const React = {
 	createElement,
 	render,
