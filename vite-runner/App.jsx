42ebedf7ce4ab655785a14945d9a8bf00a6a8e5e
sleepyheadp <17863965581@163.com>
2024-01-17 20:54:57 +0800

    feat: 04 update props

--- vite-runner/App.jsx
+++ vite-runner/App.jsx
@@ -1,18 +1,20 @@
 import React from "./core/react.js";
-function Counter({ num }) {
+let count = 10;
+function Counter() {
 	// 这个时候已经绑定了事件,但是为什么没有打印log呢?
 	// 这是因为我们没有在react.js中进行处理,它不能够识别clike这个事件
-	function handleClick(){
-		console.log("clikeMe")
+	function handleClick() {
+		count++;
+		console.log(count);
+		React.update(); // 更新
 	}
-	return <button onClick={handleClick}>count:{num}</button>;
+	return <button onClick={handleClick}>count:{count}</button>;
 }
 function App() {
 	return (
 		<div id="app">
 			Hello mini-react
-			{/* 问题:为什么要调用两次?一次性传递两个不行吗? */}
-			<Counter num={10}></Counter>
+			<Counter></Counter>
 			{/* <Counter num={20}></Counter> */}
 		</div>
 	);
--- vite-runner/core/react.js
+++ vite-runner/core/react.js
@@ -38,6 +38,7 @@ function render(el, container) {
 
 // 首先我们定义一个当前要执行的任务
 let rootDom = null; // dom树的根节点
+let currentRoot = null;
 let nextWorkOfUnit = null;
 const workLoop = (idleDeadline) => {
 	let hasTimeEmpty = false; // 默认没有空闲时间
@@ -56,58 +57,95 @@ const workLoop = (idleDeadline) => {
 
 function commitRoot() {
 	commitWork(rootDom.child);
+	currentRoot = rootDom;
 	rootDom = null;
 }
+
 function commitWork(fiber) {
 	if (!fiber) return; // 如果后续没有任务了,则直接返回
-	// 问题:这里就有一个问题,我们的函数组件并没有dom属性,因此页面会出现null字符
-	// 解决办法:我们需要对是否存在dom属性进行判断
 	let fiberParent = fiber.parent;
-	// 这里还有一个小问题?只查找一层父级是不够的,可能有多个dom进行嵌套.
-	// 需要层层向上查找,直到找到一个有dom属性的fiber节点
 	while (!fiberParent.dom) {
 		fiberParent = fiberParent.parent;
 	}
-	// 问题:函数组件没有dom属性,因此我们需要对其进行判断
-	if (fiber.dom) {
-		fiberParent.dom.append(fiber.dom);
+
+	// 判断tag类型
+	if (fiber.effectTag === "update") {
+		updateProps(fiber.dom, fiber.props, fiber.alternate?.props);
+	} else if (fiber.effectTag === "placement") {
+		if (fiber.dom) {
+			fiberParent.dom.append(fiber.dom);
+		}
 	}
+
 	commitWork(fiber.child);
 	commitWork(fiber.sibling);
 }
-// 抽离createDom
+
 function createDom(type) {
 	return type === "TEXT_ELEMENT"
 		? document.createTextNode("")
 		: document.createElement(type);
 }
-// 抽离处理props函数
-function updateProps(dom, props) {
-	Object.keys(props).forEach((key) => {
+
+function updateProps(dom, nextProps, prevProps) {
+	// 分情况进行操作
+	// 1. old有 new没有 删除
+	Object.keys(prevProps).forEach((key) => {
 		if (key !== "children") {
-			if(key.startsWith('on')){
-				const eventType = key.slice(2).toLowerCase();
-				dom.addEventListener(eventType, props[key]); // 这里的props[key]就是我们onclike的回调函数clickMe
-			}else{
-				dom[key] = props[key];
+			if (!(key in nextProps)) {
+				dom.removeAttribute(key);
+			}
+		}
+	});
+	// 2. new有 old没有 增加
+	// 3. new有 old有   修改
+	Object.keys(nextProps).forEach((key) => {
+		if (key !== "children") {
+			if (nextProps[key] !== prevProps[key]) {
+				if (key.startsWith("on")) {
+					const eventType = key.slice(2).toLowerCase();
+					dom.removeEventListener(eventType, prevProps[key]);
+					dom.addEventListener(eventType, nextProps[key]); // 这里的props[key]就是我们onclike的回调函数clickMe
+				} else {
+					dom[key] = nextProps[key];
+				}
 			}
 		}
 	});
 }
-// 转换链表
 function initChildren(fiber, children) {
-	// const eventType = fiber.props
+	let oldFiber = fiber.alternate?.child;
 	let prevChild = null;
 	children.forEach((child, index) => {
-		// 创建一个新的fiber节点,给其设置相关的属性
-		const newFiber = {
-			type: child.type,
-			props: child.props,
-			child: null,
-			parent: fiber,
-			sibling: null,
-			dom: null,
-		};
+		const isSameType = oldFiber && oldFiber.type === child.type;
+		let newFiber;
+		if (isSameType) {
+			// update
+			newFiber = {
+				type: child.type,
+				props: child.props,
+				child: null,
+				parent: fiber,
+				sibling: null,
+				dom: oldFiber.dom,
+				effectTag: "update", // 更新
+				alternate: oldFiber,
+			};
+		} else {
+			newFiber = {
+				type: child.type,
+				props: child.props,
+				child: null,
+				parent: fiber,
+				sibling: null,
+				dom: null,
+				effectTag: "placement", // 替换
+			};
+		}
+		// 把旧的dom树更新为兄弟节点
+		if (oldFiber) {
+			oldFiber = oldFiber.sibling;
+		}
 		if (index === 0) {
 			fiber.child = newFiber;
 		} else {
@@ -116,72 +154,52 @@ function initChildren(fiber, children) {
 		prevChild = newFiber;
 	});
 }
-// 抽离创建dom节点函数
+
 function updateFunctionComponent(fiber) {
 	const children = [fiber.type(fiber.props)];
 	initChildren(fiber, children);
 }
-// 抽离处理props函数
+
 function updateHostComponent(fiber) {
-	// 1. 创建dom节点
 	if (!fiber.dom) {
 		const dom = (fiber.dom = createDom(fiber.type));
-		// fiber.parent.dom.append(dom); // 将创建的dom节点添加到父节点中,但是没渲染完所有dom节点就中途添加了
-		// 2. 处理props
-		updateProps(dom, fiber.props);
+		updateProps(dom, fiber.props, {});
 	}
 	const children = fiber.props.children;
 	initChildren(fiber, children);
 }
+
 const performWorkOfUnit = (fiber) => {
-	// 检查一下fiber.type是否是一个函数组件
 	const isFunctionComponent = typeof fiber.type === "function";
 	if (isFunctionComponent) {
 		updateFunctionComponent(fiber);
 	} else {
 		updateHostComponent(fiber);
 	}
-	// 因为我们的函数组件本身就自带dom,所以并不需要在对函数组件进行createDom的操作
-	// if (!isFunctionComponent) {
-	// 	// 在这个函数中我们要做的事情就是执行当前任务,也就是render函数所做的事情
-	// 	// 1. 创建dom节点
-	// 	if (!fiber.dom) {
-	// 		const dom = (fiber.dom = createDom(fiber.type));
-	// 		// fiber.parent.dom.append(dom); // 将创建的dom节点添加到父节点中,但是没渲染完所有dom节点就中途添加了
-	// 		// 2. 处理props
-	// 		updateProps(dom, fiber.props);
-	// 	}
-	// }
-
-	// 3. 转换链表 设置引用/指针(指向下一个任务)
-	// 在init子节点的时候把判断完的children传递过去,又因为我们的children默认是[],所以需要设置为数组类型的数据
-	// const children = isFunctionComponent
-	// 	? [fiber.type(fiber.props)]
-	// 	: fiber.props.children;
-	// initChildren(fiber, children);
-	// 4. 返回下一个要执行的任务(指针指向的下一个任务)
-	// 4.1 如果当前任务有子节点,则返回子节点
 	if (fiber.child) {
 		return fiber.child;
 	}
-	// 4.2 如果当前任务没有子节点,则返回兄弟节点
-	// if (fiber.sibling) {
-	// 	return fiber.sibling;
-	// }
 
 	let nextFiber = fiber;
 	while (nextFiber) {
 		if (nextFiber.sibling) return nextFiber.sibling;
 		nextFiber = nextFiber.parent;
 	}
-
-	// 4.3 如果当前任务没有兄弟节点,则返回父节点的兄弟节点
-	// return fiber.parent?.sibling;
 };
 requestIdleCallback(workLoop);
+
+function update() {
+	nextWorkOfUnit = {
+		dom: currentRoot.dom,
+		props: currentRoot.props,
+		alternate: currentRoot, // 把新dom树指向老的dom树
+	};
+	rootDom = nextWorkOfUnit;
+}
 
 const React = {
 	createElement,
 	render,
+	update,
 };
 export default React;
